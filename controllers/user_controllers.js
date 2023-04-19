dea06a78d5481a65b9aa98fab5aa65bc04665654
Akash Padampalle <akashpadampalle780@gmail.com>
2023-04-19 15:34:48 +0530

    add user controller and login and signup routes

---
+++ controllers/user_controllers.js
@@ -0,0 +1,36 @@
+const User = require('../models/user');
+
+module.exports.signup = async function (req,res){
+    try {
+        const { name , email, password, cpassword } = req.body;
+        
+        //check if any field is empty
+        if(!name || !email || !password){
+            console.log(req.body);
+            throw new Error('Error: Empty field recieved --> signup');
+        }
+
+        // check password and confirm password are the same
+        if(password != cpassword){
+            throw new Error('Error: password != confirm password --> signup');
+        }
+
+        const createdUser = await User.create({name, email, password});
+
+        // checking user is created successfully or not
+        if(!createdUser){
+            throw new Error('Error: not able to create an user --> signup');
+        }
+
+        return res.redirect('/login');
+
+    } catch (error) {
+        console.error("Error: ", error);
+        return res.redirect('/signup');
+    }
+}
+
+
+module.exports.login = function(req, res){
+    res.render('home');
+}
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const homeController = require('../controllers/home_controller');
+const userController = require('../controllers/user_controllers');
 
 router.get('/', homeController.home);
 router.get('/login', homeController.login);
 router.get('/signup', homeController.signup);
+
+
+router.post('/login', userController.login);
+router.post('/signup', userController.signup)
 
 module.exports = router;
